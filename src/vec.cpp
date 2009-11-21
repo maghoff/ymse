ea5a4570d5359a18d17e910e3a0dcd317457765f
maghoff <unknown>
2009-11-21 13:22:14 +0100

    Capitalized template parameter. Added instantiation of vec<2, int>

--- src/vec.cpp
+++ src/vec.cpp
@@ -4,57 +4,59 @@
 
 namespace ymse {
 
-template <int dim, typename T>
-vec<dim, T>::vec() {
+template <int Dim, typename T>
+vec<Dim, T>::vec() {
 }
 
-template <int dim, typename T>
-vec<dim, T>::vec(T x, T y) {
-	assert(dim == 2);
+template <int Dim, typename T>
+vec<Dim, T>::vec(T x, T y) {
+	assert(Dim == 2);
 	v[0] = x;
 	v[1] = y;
 }
 
-template <int dim, typename T>
-vec<dim, T>::vec(T x, T y, T z) {
-	assert(dim == 3);
+template <int Dim, typename T>
+vec<Dim, T>::vec(T x, T y, T z) {
+	assert(Dim == 3);
 	v[0] = x;
 	v[1] = y;
 	v[2] = z;
 }
 
-template <int dim, typename T>
-vec<dim, T>& vec<dim, T>::operator += (vec<dim, T> rhs) {
-	for (int i=0; i<dim; ++i) v[i] += rhs[i];
+template <int Dim, typename T>
+vec<Dim, T>& vec<Dim, T>::operator += (vec<Dim, T> rhs) {
+	for (int i=0; i<Dim; ++i) v[i] += rhs[i];
 	return *this;
 }
 
-template <int dim, typename T>
-vec<dim, T>& vec<dim, T>::operator -= (vec<dim, T> rhs) {
-	for (int i=0; i<dim; ++i) v[i] -= rhs[i];
+template <int Dim, typename T>
+vec<Dim, T>& vec<Dim, T>::operator -= (vec<Dim, T> rhs) {
+	for (int i=0; i<Dim; ++i) v[i] -= rhs[i];
 	return *this;
 }
 
-template <int dim, typename T>
-vec<dim, T>& vec<dim, T>::operator *= (T rhs) {
-	for (int i=0; i<dim; ++i) v[i] *= rhs;
+template <int Dim, typename T>
+vec<Dim, T>& vec<Dim, T>::operator *= (T rhs) {
+	for (int i=0; i<Dim; ++i) v[i] *= rhs;
 	return *this;
 }
 
-template <int dim, typename T>
-T vec<dim, T>::length() const {
+template <int Dim, typename T>
+T vec<Dim, T>::length() const {
 	return sqrt(square_length());
 }
 
-template <int dim, typename T>
-T vec<dim, T>::square_length() const {
+template <int Dim, typename T>
+T vec<Dim, T>::square_length() const {
 	T acc(0);
-	for (int i=0; i<dim; ++i) acc += v[i] * v[i];
+	for (int i=0; i<Dim; ++i) acc += v[i] * v[i];
 	return acc;
 }
 
 
 template struct vec<2, float>;
 template struct vec<3, float>;
+
+template struct vec<2, int>;
 
 }
--- ymse/vec.hpp
+++ ymse/vec.hpp
@@ -3,11 +3,11 @@
 
 namespace ymse {
 
-template <int dim, typename T>
+template <int Dim, typename T>
 struct vec {
 	typedef T value_type;
 
-	T v[dim];
+	T v[Dim];
 	T operator[] (int n) const { return v[n]; }
 	T& operator[] (int n) { return v[n]; }
 
@@ -15,10 +15,10 @@ struct vec {
 	vec(T x, T y);
 	vec(T x, T y, T z);
 
-	vec<dim, T>& operator += (vec<dim, T>);
-	vec<dim, T>& operator -= (vec<dim, T>);
+	vec<Dim, T>& operator += (vec<Dim, T>);
+	vec<Dim, T>& operator -= (vec<Dim, T>);
 
-	vec<dim, T>& operator *= (T);
+	vec<Dim, T>& operator *= (T);
 
 	T length() const;
 	T square_length() const;
@@ -27,8 +27,12 @@ struct vec {
 extern template struct vec<2, float>;
 extern template struct vec<3, float>;
 
+extern template struct vec<2, int>;
+
 typedef vec<2, float> vec2f;
 typedef vec<3, float> vec3f;
+
+typedef vec<2, int> vec2i;
 
 }
 
