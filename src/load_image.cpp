6885c5b11222a7c67073946c4510590ac84acd54
maghoff <unknown>
2009-11-26 22:12:43 +0100

    Added a really prelimenary helper function for loading image files into GL textures

---
+++ src/load_image.cpp
@@ -0,0 +1,23 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <SDL_image.h>
+#include <string>
+//#include "load_image.hpp"
+
+GLuint load_texture(const std::string& file) {
+	SDL_Surface* surface = IMG_Load(file.c_str());
+	GLuint texture;
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	SDL_PixelFormat *format = surface->format;
+	if (format->Amask) {
+		gluBuild2DMipmaps(GL_TEXTURE_2D, 4, surface->w, surface->h, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+	} else {
+		gluBuild2DMipmaps(GL_TEXTURE_2D, 3, surface->w, surface->h, GL_RGB, GL_UNSIGNED_BYTE, surface->pixels);
+	}
+
+	SDL_FreeSurface(surface);
+	return texture;
+}
---
+++ ymse/load_image.hpp
@@ -0,0 +1,9 @@
+#ifndef LOAD_IMAGE_HPP
+#define LOAD_IMAGE_HPP
+
+#include <GL/gl.h>
+#include <string>
+
+GLuint load_texture(const std::string& file);
+
+#endif // LOAD_IMAGE_HPP
