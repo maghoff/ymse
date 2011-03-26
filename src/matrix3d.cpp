78ab90f93d4f33b179eb0c3970ebf5b6a513bdc2
maghoff <unknown>
2011-03-27 00:56:55 +0100

    Fixed rotate_[xyz]-functions

--- src/matrix3d.cpp
+++ src/matrix3d.cpp
@@ -13,23 +13,23 @@ matrix33f identity() {
 }
 
 matrix33f rotate_x(double th) {
-	matrix33f m(0);
+	matrix33f m(identity());
 	m[1][1] =  cos(th); m[1][2] =  sin(th);
 	m[2][1] = -sin(th); m[2][2] =  cos(th);
 	return m;
 }
 
 matrix33f rotate_y(double th) {
-	matrix33f m(0);
+	matrix33f m(identity());
 	m[2][2] =  cos(th); m[2][0] =  sin(th);
 	m[0][2] = -sin(th); m[0][0] =  cos(th);
 	return m;
 }
 
 matrix33f rotate_z(double th) {
-	matrix33f m(0);
-	m[0][0] =  cos(th); m[0][0] =  sin(th);
-	m[1][0] = -sin(th); m[1][0] =  cos(th);
+	matrix33f m(identity());
+	m[0][0] =  cos(th); m[0][1] =  sin(th);
+	m[1][0] = -sin(th); m[1][1] =  cos(th);
 	return m;
 }
 
