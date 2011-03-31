11da047398f22bbfd48bd7ef5d12686336568ea0
maghoff <unknown>
2011-03-31 19:36:06 +0200

    Added graphics to 01

--- tutorial/01_simple_window/main.cpp
+++ tutorial/01_simple_window/main.cpp
@@ -1,3 +1,12 @@
+/* This program will draw a diamond.
+ *
+ * It supports resizing the window, and will close normally if the
+ * window is closed by clicking the close-button on the window chrome.
+ *
+ * The diamond will be squished to fit the window, OpenGL-style.
+ * The coordinates inside the window always span from (-1, -1) to (1, 1).
+ */
+
 // Different platforms require different includes for OpenGL.
 // ymse/gl.h smooths over this. It should be included early.
 #include <ymse/gl.h>
@@ -9,6 +18,15 @@
 class Game : public ymse::game {
 public:
 	void render() {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// I know, I know. Old style OpenGL...
+		glBegin(GL_LINE_LOOP);
+		glVertex2f(0, 1);
+		glVertex2f(1, 0);
+		glVertex2f(0, -1);
+		glVertex2f(-1, 0);
+		glEnd();
 	}
 };
 
