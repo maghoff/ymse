bf996fbc95566092bfdecad1f8a704903fd8ed10
Magnus Hoff <maghoff@gmail.com>
2011-04-27 14:13:01 +0200

    Include gl.h properly on Windows as well

--- ymse/gl.h
+++ ymse/gl.h
@@ -1,9 +1,26 @@
 #ifndef YMSE_GL_H
 #define YMSE_GL_H
 
+#ifndef GL_GLEXT_PROTOTYPES
+#define GL_GLEXT_PROTOTYPES
+#endif
+
 #ifdef __MACH__
 
 #include <OpenGL/gl.h>
+
+#elif defined(_WIN32)
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#include <windows.h>
+#include <GL/gl.h>
 
 #else
 
