2dc7e90bbc0c0471359aa4f984958956be7272d9
Magnus Hoff <maghoff@gmail.com>
2011-05-11 16:43:51 +0200

    Seems glew does everything we need

--- ymse/gl.h
+++ ymse/gl.h
@@ -1,27 +1,7 @@
 #ifndef YMSE_GL_H
 #define YMSE_GL_H
 
-#ifndef GL_GLEXT_PROTOTYPES
-#define GL_GLEXT_PROTOTYPES
-#endif
-
 #define GLEW_STATIC
 #include <GL/glew.h>
-
-
-#ifdef __MACH__
-
-#include <OpenGL/gl.h>
-
-#elif defined(_WIN32)
-
-#include "lean_windows.h"
-#include <GL/gl.h>
-
-#else
-
-#include <GL/gl.h>
-
-#endif
 
 #endif
