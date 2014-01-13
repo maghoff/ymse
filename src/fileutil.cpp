4c8a3d263b2fcd94436f1b0d169eef9e16fbaf1a
Magnus Hoff <maghoff@gmail.com>
2014-01-13 10:08:52 +0100

    Throw relevant exception when unable to find files

--- src/fileutil.cpp
+++ src/fileutil.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -7,6 +8,7 @@ namespace ymse {
 
 void read_entire_file(std::vector<char>& target, const std::string& filename) {
 	std::ifstream in(filename.c_str(), std::ios::binary);
+	if (!in.is_open()) throw std::runtime_error("Unable to open file: " + filename);
 
 	in.seekg(0, std::ios::end);
 	target.resize(in.tellg());
